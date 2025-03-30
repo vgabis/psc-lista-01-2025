0fe07aea4d471e8765e0529569af408a976a21d3
Gabriella <116893245+vgabis@users.noreply.github.com>
2025-03-30 18:00:05 -0300

    Ola Mundo em JAVA
    Faça um algoritmo que escreva “Olá, Mundo” na tela do console. Utilize a linguagem Java para isso.

---
+++ OlaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.olamundo;
+
+/**
+ *
+ * @author gabri
+ */
+public class OlaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
+}
